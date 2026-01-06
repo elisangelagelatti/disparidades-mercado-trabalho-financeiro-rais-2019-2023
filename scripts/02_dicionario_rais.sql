ca294f235a6337b73c1c8997dd3bb60ee2c5940a
Elisangela Gelatti <elisangelagelatti@hotmail.com>
2026-01-06 16:19:14 -0300

    Add files via upload

---
+++ scripts/02_dicionario_rais.sql
@@ -0,0 +1,9 @@
+-- Projeto: Disparidades regionais e dinâmicas do mercado de trabalho financeiro-contábil (2019–2023)
+-- Fonte: RAIS via Base dos Dados (BigQuery)
+-- Dataset: basedosdados.br_me_rais.dicionario
+
+-- ETAPA 2: Consulta ao dicionário de variáveis da RAIS
+-- Objetivo: obter a descrição e metadados das variáveis utilizadas nas análises
+
+SELECT *
+FROM `basedosdados.br_me_rais.dicionario`;
